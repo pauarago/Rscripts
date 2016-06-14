386a8cc195639a415d3a14930aed2d9abc66fa9a
parago <parago@uji.es>
2016-06-14 15:16:35 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Rscripts
-R scripts useful on my diary work.
+R scripts useful on my diary work. Tak it as it is just for remember how to do it, without any guarantee dat will work. Indeed somtimes is unfinished code just to do some scratch
---
+++ developing_steps_to_publish.R
@@ -0,0 +1,30 @@
+#Those are the steps to check a package under development befor publish it
+
+#install packages if are not installed
+install.packages(c("pack1", "pacl2"))
+
+#load library form folder
+install.packages("path to your local folder", repos = NULL, type="source")
+
+#set work espace to package location
+setwd("path to your local folder")
+
+#create import packages and export fucntions in NAMESPACE FILE
+devtools::document()
+
+library(yourackage)
+
+#check functions
+ls(package:yourackage)
+
+ls(getNamespace("yourackage"), all.names=TRUE)
+
+search("package:tyourackage")
+
+#Bifore run check, all dependencies are in namespaces and in Description file
+#in de section Depends:  or Import: (preferible)
+
+
+#check the package before publish
+devtools::check()
+
